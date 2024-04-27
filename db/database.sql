488096a8cff3d71da075010d087bc662f4083574
XanderSchaekenPXL <116871180+XanderSchaekenPXL@users.noreply.github.com>
2024-04-27 15:51:49 +0200

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE pxldb;
 \c pxldb;
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto; 
+CREATE EXTENSION IF NOT EXISTS pgcrypto; -- Load pgcrypto extension
 
 CREATE USER secadv WITH PASSWORD 'ilovesecurity';
 GRANT ALL PRIVILEGES ON DATABASE pxldb TO secadv;
@@ -16,7 +16,9 @@ CREATE TABLE users (
 
 GRANT ALL PRIVILEGES ON TABLE users TO secadv;
 
-INSERT INTO users (user_name, password) VALUES ('pxl-admin', crypt('insecureand>
-INSERT INTO users (user_name, password) VALUES ('george', crypt('iwishihadbette>
+-- Insert users with hashed passwords
+INSERT INTO users (user_name, password) VALUES ('pxl-admin', crypt('insecureandlovinit', gen_salt('bf')));
+INSERT INTO users (user_name, password) VALUES ('george', crypt('iwishihadbetteradmins', gen_salt('bf')));
 
 COMMIT;
+
