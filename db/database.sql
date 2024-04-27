04ca330f3b7930481aa2f76b03bd95ef80a9dd61
XanderSchaekenPXL <116871180+XanderSchaekenPXL@users.noreply.github.com>
2024-04-27 14:21:39 +0200

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -6,10 +6,10 @@ create user secadv with password 'ilovesecurity';
 grant all privileges on database pxldb to secadv;
 BEGIN;
 
-create table users (id serial primary key, user_name text not null unique, password text not null);
+create table users (id serial primary key, user_name text not null unique, password_hash text not null);
 grant all privileges on table users to secadv;
 
-insert into users (user_name, password) values ('pxl-admin', 'insecureandlovinit') ;
-insert into users (user_name, password) values ('george', 'iwishihadbetteradmins') ;
+insert into users (user_name, password_hash) values ('pxl-admin', 'insecureandlovinit') ;
+insert into users (user_name, password_hash) values ('george', 'iwishihadbetteradmins') ;
 
 COMMIT;
